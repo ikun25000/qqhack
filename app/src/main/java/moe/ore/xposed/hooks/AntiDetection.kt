5aad3f2988ba6aa751a3da9de0f3dedb480286ba
owo233 <ver@vip.qq.com>
2025-04-01 11:15:01 +0800

    feat: support version 9.1.65.24690

--- app/src/main/java/moe/ore/xposed/hooks/AntiDetection.kt
+++ app/src/main/java/moe/ore/xposed/hooks/AntiDetection.kt
@@ -3,6 +3,7 @@ package moe.ore.xposed.hooks
 import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
 import moe.ore.xposed.util.XPClassloader.load
+import java.lang.reflect.Method
 
 internal class AntiDetection {
 
@@ -11,22 +12,34 @@ internal class AntiDetection {
     }
 
     private fun disableNewMSF() {
-        val clz = load("com.tencent.mobileqq.msf.core.f0.b")
-        val argsClass = load("com.tencent.mobileqq.msf.core.MsfCore")
-        if (clz != null && argsClass != null) {
-            clz.declaredMethods.firstOrNull {
-                it.parameterTypes.size == 2 && it.parameterTypes[0] == argsClass && it.parameterTypes[1] == Boolean::class.java
-            }?.let { method ->
-                val hook = object : XC_MethodHook() {
-                    override fun beforeHookedMethod(param: MethodHookParam) {
-                        val isNewMSF = param.args[1] as Boolean
-                        if (isNewMSF) {
-                            param.args[1] = false
-                        }
+        val primaryClass = load("com.tencent.mobileqq.msf.core.f0.b")
+        // 暂时这样写，后续可能会改用dexKit来查找
+        val fallbackClass = load("com.tencent.mobileqq.msf.core.g0.b")
+        val msfCoreClass = load("com.tencent.mobileqq.msf.core.MsfCore")
+        var targetMethod: Method? = null
+
+        if (primaryClass != null && msfCoreClass != null) {
+            targetMethod = primaryClass.declaredMethods.firstOrNull {
+                it.parameterTypes.size == 2 && it.parameterTypes[0] == msfCoreClass && it.parameterTypes[1] == Boolean::class.java
+            }
+        }
+
+        if (targetMethod == null && fallbackClass != null && msfCoreClass != null) {
+            targetMethod = fallbackClass.declaredMethods.firstOrNull {
+                it.parameterTypes.size == 2 && it.parameterTypes[0] == msfCoreClass && it.parameterTypes[1] == Boolean::class.java
+            }
+        }
+
+        targetMethod?.let { method ->
+            val hook = object : XC_MethodHook() {
+                override fun beforeHookedMethod(param: MethodHookParam) {
+                    val isNewMSF = param.args[1] as Boolean
+                    if (isNewMSF) {
+                        param.args[1] = false
                     }
                 }
-                XposedBridge.hookMethod(method, hook)
             }
+            XposedBridge.hookMethod(method, hook)
         }
     }
 }
