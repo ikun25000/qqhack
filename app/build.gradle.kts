0b24d0fc4d2d8252bf9a7a406a95e7b582c6f249
callng <ver@vip.qq.com>
2025-04-12 21:59:50 +0800

    chore: Lower version requirements for xposed framework

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -117,7 +117,7 @@ dependencies {
 
     implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("com.github.bumptech.glide:glide:4.16.0")
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
