9635058778c22d23b45e4d5c36b0caa57b13b082
callng <ver@vip.qq.com>
2025-05-11 04:07:34 +0800

    chore: upgrade project dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,6 @@ android {
                 "kotlin/**",
                 "google/**",
                 "org/**",
-                "bsh/**",
                 "WEB-INF/**",
                 "okhttp3/**",
                 "DebugProbesKt.bin",
@@ -104,8 +103,6 @@ fun configureAppSigningConfigsForRelease(project: Project) {
 }
 
 dependencies {
-    implementation("androidx.drawerlayout:drawerlayout:1.2.0")
-
     compileOnly("de.robv.android.xposed:api:82")
 
     // 数据处理的库
@@ -114,19 +111,15 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-io-jvm:0.1.16")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-protobuf:1.8.1")
     implementation(files("libs/protobuf-java-3.5.1.jar"))
-    implementation("com.tencent:mmkv:2.2.1")
+    implementation("com.tencent:mmkv:2.2.2")
 
     // UI交互的库
     implementation("com.rengwuxian.materialedittext:library:2.1.4")
     implementation("com.github.yogkin:SettingView:1.0.8")
 
     implementation("androidx.recyclerview:recyclerview:1.4.0")
-    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.1")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.7")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
-    implementation("org.apache-extras.beanshell:bsh:2.0b6")
 }
